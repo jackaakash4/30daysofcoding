9d5d5e14f88b98b81a714c0133dd9edb91c1c99f
Aakash Shrestha <jackaakash4@gmail.com>
2022-08-26 23:13:17 +0545

    Use of functions

--- day1.js
+++ day1.js
@@ -1,2 +1,34 @@
+//Data structure array
+
 var fruits = [ "Apple", "Banana", "Orange", "Mango"];
 fruits[fruits.length] = "Kiwi";
+//it adds "Kiwi" in fruits array
+//we can also do fruits.push = "Kiwi"
+//fruits = ["Apple", "Banana", "Orange", "Mango", "Kiwi"]
+
+//use of shift();
+fruits.shift();
+//it left shift the content of fruit array
+//fruits = ["Banana", "Orange", "Mango",  "Kiwi"]
+
+//use of pop();
+fruits.pop();
+//it pops the last element of array fruits
+//it pops "kiwi"
+//fruits = ["Banana", "Orange", "Mango"]
+
+
+//use of push(element);
+fruits.push("Kera");
+//it push "Kera" to the array
+//fruit = ["Banana", "Orange", "Mango", "Kera"]
+
+//use of fruits.concat[];
+var fruits= fruits.concat["Straw", "Berry"];
+//it adds "STraw" and "Berry" to the array
+// fruits = ["Banana", "Orange", "Mango", "Kera", "Straw", "Berry"]
+
+// use of sort function
+fruits.sort();
+//it sorts the element of fruits array
+
