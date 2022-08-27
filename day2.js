5d512618cd37257777e8cd737669e98119ada6af
Aakash Shrestha <jackaakash4@gmail.com>
2022-08-27 22:48:59 +0545

    DOM events

---
+++ day2.js
@@ -0,0 +1,12 @@
+//DOM events
+//most common 
+
+//click
+var button = document.getElementsByTagName("button")[0];
+button.addEventListener("click", function(){
+    console.log("Click!!");
+})
+//when we click button we see "Click!!" in console
+// another example of event like click is "mouseleave"
+
+ 
