b0eb6412fd7dacfa73cb0210b7224f512088a88d
Aakash Shrestha <jackaakash4@gmail.com>
2022-08-27 23:18:06 +0545

    Click and Enter event is added

--- day2another.js
+++ day2another.js
@@ -1,7 +1,27 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function InputLength(){ //funciton to get length of input value
+    return input.value.length;
+}
+
+function createListElement(){   //funciton to create list element and add imput value to list
+    var li = document.createElement("li");  //new li tag is created
+    li.appendChild(document.createTextNode(input.value)); //"input" text is added to li tag
+    ul.appendChild(li); //li is attached with ul tag  
+    input.value ="";
+}
 
 button.addEventListener("click", function(){
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode("testing"));
+    if(InputLength()> 0){ //condition because if we input sth it add to list and else it doesn't
+    createListElement();
+    }     
+})
+
+input.addEventListener("keypress", function(event){
+    
+    if(InputLength() > 0 && event.keyCode === 13){ //condition because if we input sth it add to list and else it doesn't
+        createListElement();
+    }     
 })
