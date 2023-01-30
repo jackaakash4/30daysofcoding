a47a03e9b9d5bdb3c2875834e2d64de0a11c314e
Aakash Shrestha <jackaakash4@gmail.com>
2023-01-30 13:59:03 +0545

    es7

---
+++ es7.js
@@ -0,0 +1,20 @@
+//new in es7
+'Hellooooooo'.includes('o')
+//true
+
+const pets = ['cat', 'dog', 'bat'];
+pets.includes('cat');
+//true
+pets.includes('beer');
+//false
+
+
+//another change
+const square = (x) => x**2;
+square(2);
+//4
+
+const cube = (x) => x**3;
+cube(3);
+//27
+
