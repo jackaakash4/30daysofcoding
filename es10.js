a003646059b55aceca9be1789d367230b5b7ecdd
Aakash Shrestha <jackaakash4@gmail.com>
2023-02-06 14:12:56 +0545

    es10 learning

--- es10.js
+++ es10.js
@@ -7,3 +7,21 @@ array.flat();
 
 nestarray.flat();   //no nested
 morearray.flat();   //[5] will not be flat
+morearray.flat(2);  //[5] will also be flat
+
+  
+const entries = ['jack', 'aakash',,,,,,,'jacky']
+entries.flat(); //it will clear the data
+
+//flatMap()
+//it will add add 'Shrestha' with the entries
+const fullName = entries.flatMap(name => name + ' Shrestha')
+fullName;
+
+//another example
+//trimStart and trimEnd
+//it will trim start and end of string
+userEmail = '       jackaakash4@gmail.com';
+userEmail2 = 'jackaakash4@gmail.com         ';
+console.log(userEmail.trimStart())
+console.log(userEmail2.trimEnd())
